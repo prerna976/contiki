9bc7db420bb2c29f56629dd13b995ba508b69d3a
Gaëtan Harter <gaetan.harter@inria.fr>
2016-12-13 15:32:09 +0100

    er-coap-dtls: adapt to the previous commit cleanup

--- apps/er-coap/er-coap-dtls.c
+++ apps/er-coap/er-coap-dtls.c
@@ -2,7 +2,6 @@
 #include "contiki-net.h"
 #include "er-coap.h"
 #include "er-coap-engine.h"
-#include "er-coap-dtls.h"
 
 #include "dtls.h"
 
@@ -32,6 +31,8 @@
 
 /*---------------------------------------------------------------------------*/
 
+static struct dtls_context_t *dtls_ctx = NULL;
+
 static int
 send_to_peer(struct dtls_context_t *ctx,
              session_t *session, uint8 *data, size_t len);
@@ -39,7 +40,71 @@ send_to_peer(struct dtls_context_t *ctx,
 static int
 read_from_peer(struct dtls_context_t *ctx,
                session_t *session, uint8 *data, size_t len);
+static int
+get_psk_info(struct dtls_context_t *ctx, const session_t *session,
+             dtls_credentials_type_t type,
+             const unsigned char *id, size_t id_len,
+             unsigned char *result, size_t result_length);
 
+static dtls_handler_t dtls_cb = {
+  .write = send_to_peer,
+  .read = read_from_peer,
+  .event = NULL,
+#ifdef DTLS_PSK
+  .get_psk_info = get_psk_info,
+#endif
+#ifdef DTLS_ECC
+  .get_ecdsa_key = NULL,
+  .verify_ecdsa_key = NULL,
+#endif
+};
+
+/*-----------------------------------------------------------------------------------*/
+void
+coap_init_communication_layer(uint16_t port)
+{
+  struct uip_udp_conn *udp_conn = NULL;
+
+  dtls_init();
+  dtls_set_log_level(DTLS_LOG_DEBUG);
+
+  udp_conn = udp_new(NULL, 0, NULL);
+  udp_bind(udp_conn, port);
+
+  dtls_ctx = dtls_new_context(udp_conn);
+  if(dtls_ctx) {
+    dtls_set_handler(dtls_ctx, &dtls_cb);
+  }
+  /* new connection with remote host */
+  printf("COAP-DTLS listening on port %u\n", uip_ntohs(udp_conn->lport));
+}
+/*-----------------------------------------------------------------------------------*/
+void
+coap_send_message(uip_ipaddr_t *addr, uint16_t port, uint8_t *data, uint16_t length)
+{
+  session_t session;
+
+  dtls_session_init(&session);
+  uip_ipaddr_copy(&session.addr, addr);
+  session.port = port;
+
+  dtls_write(dtls_ctx, &session, data, length);
+}
+/*-----------------------------------------------------------------------------------*/
+void
+coap_handle_receive()
+{
+  session_t session;
+
+  if(uip_newdata()) {
+    dtls_session_init(&session);
+    uip_ipaddr_copy(&session.addr, &UIP_IP_BUF->srcipaddr);
+    session.port = UIP_UDP_BUF->srcport;
+
+    dtls_handle_message(dtls_ctx, &session, uip_appdata, uip_datalen());
+  }
+}
+/* DTLS Specific functions */
 /*-----------------------------------------------------------------------------------*/
 #ifdef DTLS_PSK
 /* This function is the "key store" for tinyDTLS. It is called to
@@ -95,37 +160,6 @@ get_psk_info(struct dtls_context_t *ctx, const session_t *session,
 }
 #endif
 /*-----------------------------------------------------------------------------------*/
-context_t *
-coap_init_communication_layer(uint16_t port)
-{
-  static dtls_handler_t cb = {
-    .write = send_to_peer,
-    .read = read_from_peer,
-    .event = NULL,
-#ifdef DTLS_PSK
-    .get_psk_info = get_psk_info,
-#endif
-#ifdef DTLS_ECC
-    .get_ecdsa_key = NULL,
-    .verify_ecdsa_key = NULL,
-#endif
-  };
-  context_t *ctx;
-
-  struct uip_udp_conn *server_conn = udp_new(NULL, 0, NULL);
-  udp_bind(server_conn, port);
-
-  dtls_set_log_level(DTLS_LOG_DEBUG);
-
-  ctx = dtls_new_context(server_conn);
-  if(ctx) {
-    dtls_set_handler(ctx, &cb);
-  }
-  /* new connection with remote host */
-  printf("COAP-DTLS listening on port %u\n", uip_ntohs(server_conn->lport));
-  return ctx;
-}
-/*-----------------------------------------------------------------------------------*/
 static int
 send_to_peer(struct dtls_context_t *ctx,
              session_t *session, uint8 *data, size_t len)
@@ -145,18 +179,6 @@ send_to_peer(struct dtls_context_t *ctx,
   return len;
 }
 /*-----------------------------------------------------------------------------------*/
-void
-coap_send_message(context_t *ctx, uip_ipaddr_t *addr, uint16_t port, uint8_t *data, uint16_t length)
-{
-  session_t session;
-
-  dtls_session_init(&session);
-  uip_ipaddr_copy(&session.addr, addr);
-  session.port = port;
-
-  dtls_write(ctx, &session, data, length);
-}
-/*-----------------------------------------------------------------------------------*/
 static int
 read_from_peer(struct dtls_context_t *ctx,
                session_t *session, uint8 *data, size_t len)
@@ -165,18 +187,4 @@ read_from_peer(struct dtls_context_t *ctx,
   memmove(uip_appdata, data, len);
   coap_receive(ctx);
   return 0;
-}
-/*-----------------------------------------------------------------------------------*/
-void
-coap_handle_receive(context_t *ctx)
-{
-  session_t session;
-
-  if(uip_newdata()) {
-    dtls_session_init(&session);
-    uip_ipaddr_copy(&session.addr, &UIP_IP_BUF->srcipaddr);
-    session.port = UIP_UDP_BUF->srcport;
-
-    dtls_handle_message(ctx, &session, uip_appdata, uip_datalen());
-  }
 }
